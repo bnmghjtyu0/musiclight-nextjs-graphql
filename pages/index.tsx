f85fd8e015428739a168716dd0a57107732ede02
richard.liao <richard.liao@tpisoftware.com>
2023-11-16 16:02:33 +0800

    chore: fetch api 測試用

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
-const Home = () => (
+interface Data {
+  testingData: string;
+}
+
+const Home = ({ data }: { data: Data }) => (
   <>
     <h1>首頁</h1>
+
+    {data.testingData}
   </>
 );
 
+Home.getInitialProps = async () => {
+  const data = await apiCall();
+  return { data };
+};
+
 export default Home;
+
+const apiCall = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ testingData: "Just some testing data" });
+    }, 5000);
+  });
