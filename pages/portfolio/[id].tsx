65a1568263ebcee62d3326c7ecbfb6723c8b288c
richard.liao <richard.liao@tpisoftware.com>
2023-11-23 16:42:50 +0800

    chore: 使用 useLazyQuery 取得 apollo 資料

--- pages/portfolio/[id].tsx
+++ pages/portfolio/[id].tsx
@@ -1,11 +1,29 @@
-import { PortfolioApi } from "@/core/services/api/portfolio";
+import { GET_PORTFOLIO } from "@/apollo/queries";
+import { Portfolio } from "@/core/models/api/portfolio.model";
+import { useLazyQuery } from "@apollo/react-hooks";
+import { useEffect, useState } from "react";
 
-const PortfolioDetail = ({ query, portfolio }: any) => {
+const PortfolioDetail = ({ query }: any) => {
   const id = query.id;
+  const [portfolio, setPortfolio] = useState<Portfolio | null>(null);
+  const [getPortfolio, { loading, data }] = useLazyQuery(GET_PORTFOLIO);
+
+  useEffect(() => {
+    getPortfolio({ variables: { id } });
+  }, []);
+
+  if (data && !portfolio) {
+    setPortfolio(data.portfolio);
+  }
+
+  if (loading || !portfolio) {
+    return <span>Loading ...</span>;
+  }
+
   return (
     <>
       <h1>Iam detail page with ID: {id} </h1>
-      {JSON.stringify(portfolio)}
+      <p>title: {portfolio.title}</p>
     </>
   );
 };
@@ -15,8 +33,6 @@ PortfolioDetail.getInitialProps = async ({
 }: {
   query: { id: string };
 }) => {
-  const portfolioApi = new PortfolioApi();
-  const portfolio = await portfolioApi.fetchPortfolioById(query.id);
-  return { query, portfolio };
+  return { query };
 };
 export default PortfolioDetail;
