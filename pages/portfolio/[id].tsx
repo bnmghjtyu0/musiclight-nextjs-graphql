367a8f743da784932051e47a1b7a44e28c79b4d7
richard.liao <richard.liao@tpisoftware.com>
2023-11-27 17:10:23 +0800

    chore: 加入 mui

--- pages/portfolio/[id].tsx
+++ pages/portfolio/[id].tsx
@@ -1,6 +1,14 @@
 import { useGetPortfolio } from "@/apollo/actions";
 import withApollo from "@/core/hoc/withApollo";
 import { getDataFromTree } from "@apollo/react-ssr";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+import { useState } from "react";
 
 interface Props {
   query: {
@@ -10,13 +18,40 @@ interface Props {
 
 const PortfolioDetail: any = ({ query }: Props) => {
   const { data } = useGetPortfolio(query.id);
+  const [age, setAge] = useState<string>("10");
 
   const portfolio = (data && data.portfolio) || {};
+
+  const handleChange = (event: SelectChangeEvent) => {
+    const age = event.target.value;
+    setAge(age);
+  };
+
+  const onSubmit = (d) => {
+    console.log(d);
+  };
 
   return (
     <>
       <h1>Iam detail page with ID: {query.id} </h1>
       <p>title: {portfolio.title}</p>
+      <form onSubmit={onSubmit}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            value={age}
+            label="Age"
+            onChange={handleChange}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+
+        <button type="submit">送出</button>
+      </form>
     </>
   );
 };
